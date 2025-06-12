e574090ee4ae61a835a079f84fdb1332ddd46cab
Willian Amorim <willian.c.amorim@outlook.com>
2025-06-12 19:03:08 -0300

    atualização exer

---
+++ capitulo-04/js/ex4_4.js
@@ -0,0 +1,15 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector ("h2")
+
+frm.addEventListener ("submit", (e) => {
+  const hora = Number (frm.inHora.value)
+    let fuso = hora + 5
+      if (fuso > 24) {
+        fuso = fuso - 24
+      }
+
+
+resp.innerText = `Hora na França: ${fuso.toFixed(2)} Horas.(m)`
+
+  e.preventDefault()
+})

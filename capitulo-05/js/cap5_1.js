de08e2d95b6a8db182a9aced978ad164df50aace
Willian Amorim <willian.c.amorim@outlook.com>
2025-06-13 20:12:54 -0300

    atualização

--- capitulo-05/js/cap5_1.js
+++ capitulo-05/js/cap5_1.js
@@ -1,10 +1,19 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("pre")
 
-frm.addEventListener("Submit" (e)=>{
+frm.addEventListener("submit", (e)=>{
+  e.preventDefault ()
+  const numero = Number (frm.inNumero.value)
 
+  let resposta = ""
+
+  for (let i = 1; i <= 10; i= i + 1) {
+    resposta = resposta + numero + " x " + i + " = " + (numero * i) + "\n"
+  }
+
+  resp.innerText = resposta 
 })
 
-frm.addEventListener("reset", ()=>
+frm.addEventListener("reset", ()=>{
   resp.innerText = ""
 })

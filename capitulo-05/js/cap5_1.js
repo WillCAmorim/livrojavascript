aecb7da4729749d812d6fa5755861229e3ec98c0
Willian Amorim <willian.c.amorim@outlook.com>
2025-06-13 17:09:06 -0300

    atualização cap 05

---
+++ capitulo-05/js/cap5_1.js
@@ -0,0 +1,10 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("pre")
+
+frm.addEventListener("Submit" (e)=>{
+
+})
+
+frm.addEventListener("reset", ()=>
+  resp.innerText = ""
+})
